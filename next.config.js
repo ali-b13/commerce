087aec61ab971be29ae8e190d17caae0b6449e53
ali-b13 <ali322213a@gmail.com>
2024-01-31 12:20:23 +0300

    modified page on image url

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports={
                 hostname:"localhost",
                 port:"3000",
 
+            },
+            {
+                protocol:"https",
+                hostname:"arama-12121-l04hl5bo8-ali-b13.vercel.app",
+                
             }
         ]
     }
