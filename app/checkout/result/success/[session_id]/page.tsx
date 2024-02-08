13ef9bd59d63cc65777e7507669b6b0d0dd3680c
ali-b13 <ali322213a@gmail.com>
2024-02-08 12:30:41 +0300

    adding stripe webhook endpoint

--- app/checkout/result/success/[session_id]/page.tsx
+++ app/checkout/result/success/[session_id]/page.tsx
@@ -20,7 +20,7 @@ export default async function successPage({
     return notFound()
   }
   return (
-    <div className="h-[60vh] flex flex-col justify-center items-center bg-blue-100">
+    <div className="h-[80vh] flex flex-col justify-center items-center bg-blue-100">
       <div className='h-[50vh] shadow-md '>
         <title>Payment Successful</title>
         <meta name="description" content="Your payment was successful!" />
