a0a6848c1846612dd241ef780d444d788ef7d0ee
ali-b13 <ali322213a@gmail.com>
2024-02-08 17:23:22 +0300

    adding all files

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -3,14 +3,14 @@ import OrdersTable from "@/components/orders/table";
 import { Suspense } from "react";
 
 
-export default  async function page({params}:{params:{userId:string}}){
-  console.log(params,'params')
-    const orders =await getOrders(params.userId)
+export default  async function page(){
+  // console.log(params,'params')
+    // const orders =await getOrders(params.userId)
   return(
     <div className="w-full flex flex-col align-middle">
        <Suspense fallback={<div className="animate-pulse w-full bg-neutral-300 h-12"></div>}>
          <p className="font-bold text-3xl text-neutral-700 p-4">My Orders</p>
-        <OrdersTable orders={orders}/>
+        {/* <OrdersTable orders={orders}/> */}
        </Suspense>
     </div>
   )
